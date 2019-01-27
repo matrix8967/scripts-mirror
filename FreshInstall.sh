261eaa21b0e029f05e4223498a91969f74880308
Matrix8967 <matrix8967@protonmail.com>
2019-01-27 16:54:23 -0600

    Adds to .zshrc.
     - Adds Zsh, OhMyZsh, Powelevel9K, and NerdFont config to .zshrc.

--- FreshInstall.sh
+++ FreshInstall.sh
@@ -47,11 +47,66 @@ git clone https://github.com/EliverLara/Sweet.git /usr/share/themes/Sweet-Dark/
 gsettings set org.gnome.desktop.interface gtk-theme "Sweet-Dark"
 gsettings set org.gnome.desktop.wm.preferences theme "Sweet-Dark"
 
-# Install OhMyZsh, and PowerLevel9K Theme...
+# Install Zsh, OhMyZsh, PowerLevel9K Theme, and NerdFonts...
 echo -e "Installing ${GREEN}OhMyZsh${NC} and ${GREEN}PowerLevel9K${NC} Theme..."
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
 sed -i -e 's/^ZSH_THEME="robbyrussell"/ZSH_THEME="powerlevel9k\/powerlevel9k"/' ~/.zshrc
+sed -i ' 1 s/.*/&POWERLEVEL9K_MODE='nerdfont-complete'\n/' .zshrc
+cat <<EOF >> ~/.zshrc
+screenfetch
+
+function weather {
+  curl -s "http://wttr.in/${1:-Little Rock}" | head -n 27
+}
+
+## User with skull
+# user_with_skull() {
+#     echo -n "\ufb8a $(whoami)"
+# }
+# POWERLEVEL9K_CUSTOM_USER="user_with_skull"
+# POWERLEVEL9K_CUSTOM_USER_FOREGROUND="black"
+# POWERLEVEL9K_CUSTOM_USER_BACKGROUND="red"
+
+POWERLEVEL9K_CONTEXT_DEFAULT_BACKGROUND='green'
+POWERLEVEL9K_CONTEXT_DEFAULT_FOREGROUND='black'
+
+POWERLEVEL9K_TIME_FORMAT="%D{%H:%M \uf073 %m.%d.%y}"
+POWERLEVEL9K_TIME_FOREGROUND='black'
+POWERLEVEL9K_TIME_BACKGROUND='green'
+
+# Battery (laptop.)
+POWERLEVEL9K_BATTERY_VERBOSE=true
+POWERLEVEL9K_BATTERY_CHARGING='yellow'
+POWERLEVEL9K_BATTERY_CHARGED='green'
+POWERLEVEL9K_BATTERY_DISCONNECTED='$DEFAULT_COLOR'
+POWERLEVEL9K_BATTERY_LOW_THRESHOLD='10'
+POWERLEVEL9K_BATTERY_LOW_COLOR='red'
+POWERLEVEL9K_BATTERY_ICON='\uf1e6 '
+POWERLEVEL9K_BATTERY_LOW_FOREGROUND='red'
+POWERLEVEL9K_BATTERY_CHARGING_FOREGROUND='blue'
+POWERLEVEL9K_BATTERY_CHARGED_FOREGROUND='green'
+POWERLEVEL9K_BATTERY_DISCONNECTED_FOREGROUND='blue'
+
+# Left and Right Prompt/Line handling.
+
+POWERLEVEL9K_PROMPT_ON_NEWLINE=true
+POWERLEVEL9K_RPROMPT_ON_NEWLINE=false
+# POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX="%{%F{249}%}\u250f"
+# POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX="%{%F{249}%}\u2517%{%F{default}%}❯"
+# POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX=''
+POWERLEVEL9K_MULTILINE_FIRST_PROMPT_PREFIX=''
+POWERLEVEL9K_MULTILINE_LAST_PROMPT_PREFIX='\ufb0c '
+
+# POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(context dir vcs battery vpn_ip public_ip ip icons_test)
+POWERLEVEL9K_RIGHT_PROMPT_ELEMENTS=(status root_indicator background_jobs history time battery)
+POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(os_icon context dir vcs)
+POWERLEVEL9K_SHORTEN_DIR_LENGTH=2
+
+# =====Misc. Env Variables=====
+
+export GOPATH=$HOME/Git/Misc/Go/
+EOF
 
 # Setup NanoRC
 cat <<EOF > ~/.nanorc
