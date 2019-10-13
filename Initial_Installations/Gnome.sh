3e113b9ab1d307b205bfa97a501fc7021d30bb82
Matrix8967 <matrix8967@protonmail.com>
2019-10-13 13:51:19 -0500

    Testing new Package Install Methods.
     - Prompts for Each "set" of Apps.
     - New ReadWhile loop for default package installs.
       - This will be segmented out into categories later.
     - Fix typo in Gnome.sh

--- Initial_Installations/Gnome.sh
+++ Initial_Installations/Gnome.sh
@@ -7,7 +7,7 @@ GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 #VERS=$(gnome-shell  | cut -d " " -f 3)
 
-sudo apt isntall gnome-calculator gnome-logs gnome-system-monitor gnome-characters gnome-tweak-tool chrome-gnome-shell
+sudo apt install gnome-calculator gnome-logs gnome-system-monitor gnome-characters gnome-tweak-tool chrome-gnome-shell
 
 # Install Gnome Shell Extensions...(Thanks https://github.com/NicolasBernaerts/)
 
