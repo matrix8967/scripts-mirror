9661886985555636805b6af03f09abe1a3b1c239
Matrix8967 <matrix8967@protonmail.com>
2020-01-01 18:25:39 -0600

    Adjust Arch and Dev Scripts for recent restructure.

--- Configs/Kitty/kitty.sh
+++ Configs/Kitty/kitty.sh
@@ -6,7 +6,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-mkdir -p ~/.config/kitty/kitty-themes
+mkdir -p ~/.config/kitty/themes
 
-cp kitty.conf ~/.conf/kitty/kitty.conf
-cp -r kitty-themes/ ~/.conf/kitty/
+cp kitty.conf ~/.config/kitty/kitty.conf
+cp -r themes/ ~/.config/kitty/
