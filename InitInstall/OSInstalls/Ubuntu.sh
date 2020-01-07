ea9dfd18c15eae25ba2c87b90921dfe1bc5b6e40
Matrix8967 <matrix8967@protonmail.com>
2020-01-06 19:17:48 -0600

    Adjust Packages for Ubuntu.

--- InitInstall/OSInstalls/Ubuntu.sh
+++ InitInstall/OSInstalls/Ubuntu.sh
@@ -6,7 +6,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-apt=$(cat ../Pkglists/UbuntuAptPacks.txt)
+apt=$(cat Pkglists/UbuntuAptPacks.txt)
 
 # Get updated...
 echo -e ${GREEN}"Getting Updated..."${NC}
@@ -32,7 +32,7 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 echo -e "Install Flatpaks?"
 select yn in "Yes" "No"; do
     case $yn in
-        "Yes") ./FlatPaks.sh;;
+        "Yes") Pkglists/./FlatPaks.sh;;
         "No") break;;
     esac
 done
@@ -40,7 +40,7 @@ done
 echo -e "Install Dev Tools?"
 select yn in "Yes" "No"; do
     case $yn in
-        "Yes") ./Dev.sh;;
+        "Yes") Pkglists/./Dev.sh;;
         "No") break;;
     esac
 done
@@ -48,9 +48,9 @@ done
 echo -e "Install Gnome Mods?"
 select yn in "Yes" "No"; do
     case $yn in
-        "Yes") ./$gnome;;
+        "Yes") Pkglists/./$gnome;;
         "No") break;;
     esac
 done
 
-echo -e "Install DotFiles?"
+#echo -e "Install DotFiles?"
