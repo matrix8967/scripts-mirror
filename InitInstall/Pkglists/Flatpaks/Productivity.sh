ced23f252c2537e4814a1eb868320d2a6102af0d
matrix8967 <matrix8967@protonmail.com>
2020-06-05 22:21:57 -0500

    Add Dracula Ghostwriter Theme.

--- InitInstall/Pkglists/Flatpaks/Productivity.sh
+++ InitInstall/Pkglists/Flatpaks/Productivity.sh
@@ -9,3 +9,6 @@ NC='\033[0m' # No Color
 # net.daase.journable org.onlyoffice.desktopeditors net.rpdev.OpenTodoList com.github.alainm23.planner com.github.debauchee.barrier
 
 flatpak install flathub -y me.kozec.syncthingtk org.nextcloud.Nextcloud org.gnome.FeedReader com.github.johnfactotum.Foliate io.github.wereturtle.ghostwriter com.github.marktext.marktext
+
+# Copy Ghostwriter Theme.
+cp -r ../../../Configs/Misc/Ghostwriter/Dracula $HOME/.var/app/io.github.wereturtle.ghostwriter/config/ghostwriter/themes/
