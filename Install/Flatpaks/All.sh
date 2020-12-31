bb28db0b7d6ebe6bf3eceea749b9d662b56efca5
matrix8967 <matrix8967@protonmail.com>
2020-12-31 16:04:49 -0600

    Removed Bloat. Thanks Isaac. ;)

--- Install/Flatpaks/All.sh
+++ Install/Flatpaks/All.sh
@@ -6,8 +6,4 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-./Comms.sh
-./Dev.sh
-./Games.sh
-./Media.sh
-./Productivity.sh
+flatpak install flathub -y com.bitwarden.desktop org.signal.Signal im.riot.Riot com.discordapp.Discord com.github.johnfactotum.Foliate io.github.wereturtle.ghostwriter com.github.marktext.marktext org.gnome.gitlab.somas.Apostrophe md.obsidian.Obsidian
--- Install/Toolsets/Fonts.sh
+++ Install/Toolsets/Fonts.sh
@@ -10,8 +10,6 @@ NC='\033[0m' # No Color
 echo -e "Installing ${GREEN}NerdFonts, OhMyZsh${NC} and ${GREEN}PowerLevel10K${NC} Theme..."
 mkdir ~/.fonts
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Agave.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/AnonymousPro.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/BigBlueTerminal.zip -P ~/.fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/DroidSansMono.zip -P ~/.fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraCode.zip -P ~/.fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/FiraMono.zip -P ~/.fonts/
@@ -19,8 +17,6 @@ wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Hack.zip -
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/JetBrainsMono.zip -P ~/.fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Mononoki.zip -P ~/.fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/RobotoMono.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Terminus.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/Ubuntu.zip -P ~/.fonts/
 wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.1.0/UbuntuMono.zip -P ~/.fonts/
 unzip ~/.fonts/'*.zip' -d ~/.fonts/
 fc-cache
