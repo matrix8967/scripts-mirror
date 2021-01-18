e55862e7956796ded0b712f3c7797bb6cb181fd6
matrix8967 <matrix8967@protonmail.com>
2021-01-18 17:07:24 -0600

    Removes Ansible. Updates GoLang & Rust Installs.

--- Install/Toolsets/GoLang.sh
+++ Install/Toolsets/GoLang.sh
@@ -6,7 +6,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-go get github.com/cjbassi/gotop
+go get -u github.com/xxxserxxx/gotop/cmd/gotop
 go get github.com/charmbracelet/glow
 go get -u github.com/jingweno/ccat
 
--- Install/Toolsets/Rust.sh
+++ Install/Toolsets/Rust.sh
@@ -6,5 +6,10 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-cargo install lsd bat bottom
-cargo install -f --git https://github.com/cjbassi/ytop ytop
+cargo install lsd bat bottom bandwhich
+
+# Bandwhich requires `root` to probe NICs and capture packets:
+
+sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep $(which bandwhich)
+
+# cargo install -f --git https://github.com/cjbassi/ytop ytop
