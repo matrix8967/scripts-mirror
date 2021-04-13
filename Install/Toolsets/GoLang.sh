4d322adee57b7b776f98b8fc1380689d1edf8cee
matrix8967 <matrix8967@protonmail.com>
2021-04-13 14:44:06 -0500

    Touch up Android Script. Update ZSHRC.

--- Install/Toolsets/GoLang.sh
+++ Install/Toolsets/GoLang.sh
@@ -6,9 +6,12 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-go get -u github.com/xxxserxxx/gotop/cmd/gotop
+export GO111MODULES=on
+
+# go get -u github.com/xxxserxxx/gotop/cmd/gotop
 go get github.com/charmbracelet/glow
 go get -u github.com/jingweno/ccat
 go get github.com/jesseduffield/lazydocker
+go get -u github.com/vultr/vultr-cli
 
 sudo cp $GOPATH/bin/* /usr/bin/
