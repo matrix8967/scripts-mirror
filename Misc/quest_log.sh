0aaaf802e96eb1358d68c186108ebf2c60daed1d
Gonzo-Miserabilist <4941505-Gonzo-Miserabilist@users.noreply.gitlab.com>
2021-05-30 05:24:44 -0500

    Added a Quest Log. ᕙ(⇀‸↼‶)ᕗ

---
+++ Misc/quest_log.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+firefox \
+	-new-tab -url https://careers.mozilla.org/listings/\
+	-new-tab -url https://system76.com/careers/\
+	-new-tab -url https://www.torproject.org/about/jobs/\
+	-new-tab -url https://mullvad.net/en/help/jobs/\
+	-new-tab -url https://duckduckgo.com/hiring/\
+	-new-tab -url https://signal.org/workworkwork/\
+	-new-tab -url https://www.gog.com/work\
+	-new-tab -url https://puri.sm/jobs/\
+	-new-tab -url https://careers.protonmail.com/\
+	-new-tab -url https://www.eff.org/about/opportunities/jobs\
+	-new-tab -url https://apply.workable.com/elementio/\
+	-new-tab -url https://www.ifixit.com/Info/jobs/\
+	-new-tab -url https://www.linode.com/company/careers/jobs/\
+	-new-tab -url https://www.vultr.com/company/careers/\
+	-new-tab -url https://www.amd.com/en/corporate/careers/\
+	-new-tab -url https://signal.org/workworkwork/\
+	-new-tab -url https://nostarch.com/Jobs.htm/\
+	-new-tab -url https://www.fossjobs.net/jobs/\
+	-new-tab -url https://freedom.press/jobs/\
