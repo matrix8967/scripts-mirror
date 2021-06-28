b2f45bb9feed0fd1d0685e6d0747167720f2e9da
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2021-06-28 13:58:42 -0500

    Fix GoLang ENV.

--- Install/Toolsets/GoLang.sh
+++ Install/Toolsets/GoLang.sh
@@ -6,8 +6,9 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-export GO111MODULES=on
+export GO111MODULES=auto
 
+go get github.com/charmbracelet/glow
 go get -u github.com/jingweno/ccat
 go get github.com/jesseduffield/lazydocker
 go get -u github.com/vultr/vultr-cli
@@ -15,9 +16,4 @@ go get -u github.com/sachaos/tcpterm
 
 sudo cp $GOPATH/bin/* /usr/bin/
 
-git -C /home/$USER/Git/Misc/ clone https://github.com/charmbracelet/glow.git
-
-echo -e ${GREEN}"I suck at GoLang, just *GO*...build it yourself..."
-echo -e "cd /home/$USER/Git/Misc/glow/"
-echo -e "go build"
-echo -e "sudo cp glow /usr/bin/"${NC}
+# git -C /home/$USER/Git/Misc/ clone https://github.com/charmbracelet/glow.git
