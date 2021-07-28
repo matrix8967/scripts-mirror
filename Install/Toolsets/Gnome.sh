fac94eb5f3385a4526fde7133376620a3e83ebce
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2021-07-27 19:33:23 -0500

    ElecomDeftPro & Gnome Updates.

--- Install/Toolsets/Gnome.sh
+++ Install/Toolsets/Gnome.sh
@@ -6,18 +6,23 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-sudo apt install -y gnome-shell-extension-no-annoyance gnome-shell-extension-remove-dropdown-arrows gnome-shell-extensions gnome-shell-extension-system-monitor gnome-shell-extension-dash-to-panel
+# sudo apt install -y gnome-shell-extension-no-annoyance gnome-shell-extension-remove-dropdown-arrows gnome-shell-extensions gnome-shell-extension-system-monitor gnome-shell-extension-dash-to-panel
 
 mkdir /home/$USER/.themes
 mkdir /home/$USER/.icons
 
+wget --content-disposition https://github.com/dracula/gtk/archive/master.zip ~/.themes
+wget --content-disposition https://github.com/dracula/gtk/files/5214870/Dracula.zip ~/.icons
+
 # Download Eye Candy.
 
-git clone https://github.com/dracula/gtk.git ~/.themes/Dracula
 git clone https://github.com/EliverLara/Juno.git --branch ocean --single-branch ~/.themes/Juno
 git clone https://github.com/EliverLara/Kripton.git ~/.themes/Kripton
 git clone https://github.com/EliverLara/Sweet.git ~/.themes/Sweet
 git clone https://github.com/EliverLara/Ant.git ~/.themes/Ant
+
+gsettings set org.gnome.desktop.sound event-sounds false
+
 # git clone https://github.com/EliverLara/candy-icons.git ~/.icons/Candy-Icons
 # git clone https://github.com/vinceliuice/Qogir-icon-theme.git ~/.icons/Qogir-Cursors
 # git clone https://github.com/KaizIqbal/Bibata_Cursor.git ~/.icons/Bibata-Cursors
@@ -25,4 +30,4 @@ git clone https://github.com/EliverLara/Ant.git ~/.themes/Ant
 
 # Open the browser to install Gnome Extensions because Gnome sucks.
 
-firefox -new-tab -url https://extensions.gnome.org/extension/3628/arcmenu/ -new-tab -url https://extensions.gnome.org/extension/779/clipboard-indicator/ -new-tab -url https://extensions.gnome.org/extension/307/dash-to-dock/ -new-tab -url https://extensions.gnome.org/extension/545/hide-top-bar/ -new-tab -url https://extensions.gnome.org/extension/3357/material-shell/ -new-tab -url https://extensions.gnome.org/extension/800/remove-dropdown-arrows/ -new-tab -url https://extensions.gnome.org/extension/120/system-monitor/ -new-tab -url https://extensions.gnome.org/extension/1031/topicons/ -new-tab -url https://extensions.gnome.org/extension/19/user-themes/
+# firefox -new-tab -url https://extensions.gnome.org/extension/3628/arcmenu/ -new-tab -url https://extensions.gnome.org/extension/779/clipboard-indicator/ -new-tab -url https://extensions.gnome.org/extension/307/dash-to-dock/ -new-tab -url https://extensions.gnome.org/extension/545/hide-top-bar/ -new-tab -url https://extensions.gnome.org/extension/3357/material-shell/ -new-tab -url https://extensions.gnome.org/extension/800/remove-dropdown-arrows/ -new-tab -url https://extensions.gnome.org/extension/120/system-monitor/ -new-tab -url https://extensions.gnome.org/extension/1031/topicons/ -new-tab -url https://extensions.gnome.org/extension/19/user-themes/
