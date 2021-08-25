65fb32e3f502fe5b4e561aefb4ff4fa780b84df0
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2021-08-25 00:42:26 -0500

    keeb touch-ups.

--- Misc/xinput/Keeb_Keychron.sh
+++ Misc/xinput/Keeb_Keychron.sh
@@ -3,7 +3,11 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'    # No Color
 
-# Universal/Keychron:
+# lsusb:
+# ID 05ac:024f Apple, Inc. Aluminium Keyboard (ANSI)
+
+# xinput list
+# Keychron Keychron K3
 
 cat <<EOF > /etc/modprobe.d/hid_apple.conf
 options hid_apple fnmode=2
--- Misc/xinput/Keeb_TK.sh
+++ Misc/xinput/Keeb_TK.sh
@@ -3,7 +3,11 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'    # No Color
 
+# xinput list:
 # Apple Inc. Magic Keyboard with Numeric Keypad
+
+# lsusb:
+# Bus 001 Device 017: ID 05ac:026c Apple, Inc. Magic Keyboard with Numeric Keypad
 
 cat <<EOF > /etc/modprobe.d/hid_apple.conf
 options hid_apple fnmode=2
