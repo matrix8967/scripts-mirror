6213834cbc41f889de1446fccaad282462b385b9
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2021-10-05 17:14:30 -0500

    Python Packages.

---
+++ Install/Misc/Python.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+pip3 install bpytop paramiko mkdocs-material easy_install bdfr pipx protontricks
+pip3 install --user pmbootstrap
