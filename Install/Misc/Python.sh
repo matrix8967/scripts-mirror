4c46b1d9da00c0dac6dda45e8963c05d5266f9f1
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2021-10-27 08:42:36 -0500

    Python Packages. MacOS ZSHRC Updates.

--- Install/Misc/Python.sh
+++ Install/Misc/Python.sh
@@ -6,5 +6,5 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-pip3 install bpytop paramiko mkdocs-material easy_install bdfr pipx protontricks
+pip3 install bpytop paramiko mkdocs-material easy_install bdfr pipx protontricks bpytop --upgrade
 pip3 install --user pmbootstrap
