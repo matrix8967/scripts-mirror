fd3a9ca55ea7c5bec8a3cdf0392eb92e9ff6670c
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2022-03-11 00:04:54 -0600

    Update Manjaro Pkgs. Add Snappack Removal Script.

---
+++ Misc/Remove_Snaps.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -eE
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+sudo snap remove lxd
+sudo snap remove core18
+sudo snap remove snapd
+
+sudo apt autoremove --purge snapd
+
+sudo rm -rf /var/cache/snapd/
+
+cat <<EOF | sudo tee /etc/apt/preferences.d/nosnap.pref
+# To prevent repository packages from triggering the installation of Snap,
+# this file forbids snapd from being installed by APT.
+
+Package: snapd
+Pin: release a=*
+Pin-Priority: -10
+EOF
