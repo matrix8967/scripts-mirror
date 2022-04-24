2a8eaf5a38b6f0710c66cb9a6f14da329282b461
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2022-04-24 16:49:34 -0500

    Start scripting Snap Removal for Ubuntu 22.04 LTS.

--- Misc/Remove_Snaps.sh
+++ Misc/Remove_Snaps.sh
@@ -5,11 +5,14 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-sudo snap remove lxd
-# sudo snap remove core18
+sudo snap remove bare
 sudo snap remove core20
+sudo snap remove firefox
+sudo snap remove gnome-3-38-2004
+sudo snap remove gtk-common-themes
+sudo snap remove snap-store
 sudo snap remove snapd
-
+sudo snap remove snapd-desktop-integration
 sudo apt autoremove --purge snapd
 
 sudo rm -rf /var/cache/snapd/
@@ -22,3 +25,8 @@ Package: snapd
 Pin: release a=*
 Pin-Priority: -10
 EOF
+# Install Firefox via deb repo:
+# sudo add-apt-repository ppa:mozillateam/ppa
+# echo '\nPackage: *\nPin: release o=LP-PPA-mozillateam\nPin-Priority: 1001\n' | sudo tee /etc/apt/preferences.d/mozilla-firefox
+# echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
+# sudo apt install firefox
