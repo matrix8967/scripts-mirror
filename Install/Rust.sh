0db116970a495351a64bdd5ae75c9b96ee3e759e
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2023-01-09 19:10:19 -0600

    Adds UI Directory Tree. Updates Configs.
    * Lays directory tree to centralize UI Configs for DEs.
    * Updates VSCodium Profile.
    * Updates zshrc.
    * Removes Debian Package (tty-clock).
    * Updates Rust Packages.

--- Install/Rust.sh
+++ Install/Rust.sh
@@ -6,7 +6,7 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' # No Color
 
-cargo install lsd bat bottom bandwhich mdbook procs du-dust watchexec-cli ripgrep cloak checkpwn taskwarrior-tui gpg-tui gping apkeep battop cargo-update battop petname rustscan gitui
+cargo install lsd bat bottom bandwhich mdbook procs du-dust watchexec-cli ripgrep cloak checkpwn taskwarrior-tui gpg-tui gping apkeep battop cargo-update battop petname rustscan gitui ytop watchexec-cli hx cloak
 
 sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep $(which bandwhich)
 
