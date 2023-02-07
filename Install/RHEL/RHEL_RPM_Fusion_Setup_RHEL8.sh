a380f955854b0b68f57dfe9bdef1a3d272fa1a23
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2023-02-07 04:29:36 -0600

    Updates RHEL and MacOS Configs.

---
+++ Install/RHEL/RHEL_RPM_Fusion_Setup_RHEL8.sh
@@ -0,0 +1,9 @@
+#####
+## RHEL 8
+#####
+
+#!/bin/bash
+
+sudo subscription-manager repos --enable "codeready-builder-for-rhel-8-$(uname -m)-rpms" && \
+sudo dnf install --nogpgcheck https://dl.fedoraproject.org/pub/epel/epel-release-latest-$(rpm -E %rhel).noarch.rpm && \
+sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
---
+++ Install/RHEL/RHEL_RPM_Fusion_Setup_RHEL9.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m'    # No Color
+
+#####
+## RHEL 9
+#####
+
+sudo subscription-manager repos --enable codeready-builder-for-rhel-9-$(arch)-rpms && \
+sudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm && \
+sudo dnf install --nogpgcheck https://mirrors.rpmfusion.org/free/el/rpmfusion-free-release-$(rpm -E %rhel).noarch.rpm https://mirrors.rpmfusion.org/nonfree/el/rpmfusion-nonfree-release-$(rpm -E %rhel).noarch.rpm
