52a00beaf1792604f8a1ac74de91062f9cbf94b7
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2023-02-09 13:15:37 -0600

    Update Font Script.

--- Install/Misc/Fonts.sh
+++ Install/Misc/Fonts.sh
@@ -9,15 +9,15 @@ NC='\033[0m' # No Color
 # Install Zsh, OhMyZsh, PowerLevel10K Theme, and NerdFonts...
 echo -e "Installing ${GREEN}NerdFonts, OhMyZsh${NC} and ${GREEN}PowerLevel10K${NC} Theme..."
 mkdir ~/.fonts
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Agave.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/DroidSansMono.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraCode.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/FiraMono.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Hack.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/JetBrainsMono.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/Mononoki.zip -P ~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/RobotoMono.zip -P~/.fonts/
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.2.2/UbuntuMono.zip -P~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Agave.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/DroidSansMono.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraCode.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/FiraMono.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Hack.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/JetBrainsMono.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/Mononoki.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/RobotoMono.zip -P ~/.fonts/
+wget https://github.com/ryanoasis/nerd-fonts/releases/download/v2.3.3/UbuntuMono.zip -P ~/.fonts/
 unzip ~/.fonts/'*.zip' -d ~/.fonts/
 wget -O ~/.fonts/fira.tar.gz https://github.com/mozilla/Fira/archive/4.202.tar.gz
 tar xf ~/.fonts/fira.tar.gz --wildcards "*.ttf" --directory=~/.fonts
