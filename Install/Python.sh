13107944403e41097002831d95461614384ac745
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2024-12-11 05:34:09 -0600

    Adds Zed Config. Updates Rust & Python Installs.

--- Install/Python.sh
+++ Install/Python.sh
@@ -3,12 +3,17 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' 	# No Color
 
-pip3 install --user pywal --upgrade
-pip3 install --user shodan --upgrade
-pip3 install --user bdfr --upgrade
-pip3 install --user waybackpack --upgrade
-pip3 install --user pmbootstrap --upgrade
-pip3 install --user bpytop --upgrade
-pip3 install --user paramiko --upgrade
-pip3 install --user argcomplete --upgrade
-pip3 install --user tk --upgrade
+pipx install pywal
+pipx install shodan
+pipx install bdfr
+pipx install waybackpack
+pipx install pmbootstrap
+pipx install bpytop
+pipx install paramiko
+pipx install argcomplete
+pipx install tk
+pipx install terminaltexteffects
+pipx install frogmouth
+pipx install bbot
+pipx install jrnl
+pipx install maigret
--- Install/Rust.sh
+++ Install/Rust.sh
@@ -8,6 +8,87 @@ NC='\033[0m' # No Color
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
-cargo install apkeep bandwhich bat battop bottom cargo-bundle cargo-update checkpwn cloak czkawka_cli du-dust gitui gpg-tui gping hx libreddit lsd ludusavi mdbook names navi onefetch petname pfetch pixfetch procs qfetch rage rfetch ripgrep rustdesk rustscan sniffnet taskwarrior-tui toipe watchexec-cli ytop
+# cargo install apkeep bandwhich bat battop bottom cargo-bundle cargo-update checkpwn cloak czkawka_cli du-dust gitui gpg-tui gping hx libreddit lsd ludusavi mdbook names navi onefetch petname pfetch pixfetch procs qfetch rage rfetch ripgrep rustdesk rustscan sniffnet taskwarrior-tui toipe watchexec-cli ytop
+
+cargo install aarty \
+amdgpu_top \
+apkeep \
+bandwhich \
+bat \
+battop \
+bluetui \
+bottom \
+cargo-bundle \
+cargo-update \
+cfonts \
+checkpwn \
+chess-tui \
+cloak \
+csv-to-usv \
+czkawka_cli \
+daktilo \
+dipc \
+diskonaut \
+doggo \
+du-dust \
+eureka \
+fre \
+gitui \
+gpg-tui \
+gping \
+hurl \
+hx \
+imgcatr \
+impala \
+just \
+kdash \
+kmon \
+libreddit \
+logss \
+lsd \
+ludusavi \
+mdbook \
+names \
+navi \
+netop \
+oha \
+onefetch \
+osintui \
+oxker \
+petname \
+pfetch \
+pixfetch \
+presenterm \
+procs \
+projectable \
+qfetch \
+qsv \
+rage \
+rerun-cli \
+rfetch \
+rioterm \
+ripgrep \
+rustdesk \
+rustic-rs \
+rustscan \
+scryptenc-cli \
+sniffnet \
+spotifyd \
+spotify-tui \
+systeroid \
+systeroid-tui \
+tailspin \
+taskwarrior-tui \
+toipe \
+tracexec \
+trippy \
+uefisettings \
+watchexec-cli \
+xsv \
+yazi-cli \
+yazi-fm \
+ytop \
+zellij \
+zenith
 
 sudo setcap cap_sys_ptrace,cap_dac_read_search,cap_net_raw,cap_net_admin+ep $(which bandwhich)
