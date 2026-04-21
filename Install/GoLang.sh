cce45454bad1e2c19a4a9844ec5ea4ecb8513731
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2026-04-21 08:34:16 -0500

    Updates Zed, Ghostty, and ZSH configs. Updates Go, Rust, and Python packages.

--- Install/GoLang.sh
+++ Install/GoLang.sh
@@ -8,41 +8,63 @@ NC='\033[0m' # No Color
 
 export GO111MODULES=auto
 
-go install github.com/charmbracelet/glow@latest
-go install github.com/maaslalani/draw@latest
+# go get github.com/cointop-sh/cointop
 # go get -u github.com/jingweno/ccat
-go install github.com/jesseduffield/lazydocker@latest
-go install github.com/sachaos/tcpterm@latest
 # go get -u github.com/vultr/govultr/v2
+go install dns.froth.zone/awl@latest
+go install github.com/aayushbtw/monit@latest
+go install github.com/abhimanyu003/sttr@latest
+go install github.com/AkashRajpurohit/git-sync@latest
+go install github.com/AshBuk/FingerGo@latest
+go install github.com/backendsystems/nibble@latest
+go install github.com/bettercap/bettercap/v2@latest
+go install github.com/Bharath-code/git-scope/cmd/git-scope@latest
+go install github.com/BigJk/imeji/cmd/imeji@latest
+go install github.com/bjesus/pipet/cmd/pipet@latest
+go install github.com/bradfitz/embiggen-disk@latest
+go install github.com/carban/postbear@latest
+go install github.com/c-grimshaw/gosniff@latest
+go install github.com/charmbracelet/glow@latest
+go install github.com/charmbracelet/gum@latest
+go install github.com/codeshaine/curlify@latest
+go install github.com/cointop-sh/cointop@latest
+go install github.com/crgimenes/neko@latest
+go install github.com/darkhz/adbtuifm@latest
+go install github.com/darkhz/bluetuith@latest
+go install github.com/ddddddO/packemon/cmd/packemon@latest
+go install github.com/DerTimonius/twkb@latest
+go install github.com/fabio42/ssl-checker@latest
 go install github.com/gonetx/httpit@latest
+go install github.com/halftoothed/gostman@latest
+go install github.com/hjr265/gittop@latest
+go install github.com/jedisct1/go-minisign@latest
+go install github.com/jesseduffield/lazydocker@latest
 go install github.com/jesseduffield/lazygit@latest
 go install github.com/jmhobbs/terminal-parrot@latest
-# go get github.com/cointop-sh/cointop
-go install github.com/cointop-sh/cointop@latest
-go install github.com/charmbracelet/gum@latest
-go install github.com/maaslalani/gambit@latest
-go install github.com/maaslalani/cue@latest
-go install github.com/c-grimshaw/gosniff@latest
-go install github.com/maaslalani/slides@latest
-go install github.com/fabio42/ssl-checker@latest
-go install github.com/mr-karan/doggo/cmd/doggo@latest
-go install github.com/TheZoraiz/ascii-image-converter@latest
-go install github.com/AkashRajpurohit/git-sync@latest
-go install github.com/aayushbtw/monit@latest
-go install github.com/Zebbeni/ansizalizer@latest
-go install github.com/BigJk/imeji/cmd/imeji@latest
 go install github.com/jordanella/go-ansi-paintbrush@latest
-go install github.com/bradfitz/embiggen-disk@latest
-go install github.com/DerTimonius/twkb@latest
-go install github.com/ddddddO/packemon/cmd/packemon@latest
-go install github.com/abhimanyu003/sttr@latest
-go install github.com/szktkfm/mdtt/cmd/mdtt@latest
-go install github.com/darkhz/bluetuith@latest
-go install github.com/Zxilly/go-size-analyzer/cmd/gsa@latest
-go install github.com/mrusme/planor@latest
+go install github.com/karol-broda/snitch@latest
 go install github.com/Lifailon/lazyjournal@latest
+go install github.com/maaslalani/cue@latest
+go install github.com/maaslalani/draw@latest
+go install github.com/maaslalani/gambit@latest
+go install github.com/maaslalani/slides@latest
+go install github.com/mexirica/aptui@latest
+go install github.com/microsoft/ethr@latest
+go install github.com/mr-karan/doggo/cmd/doggo@latest
+go install github.com/mrusme/planor@latest
+go install github.com/natesales/q@latest
+go install github.com/oug-t/difi/cmd/difi@latest
+go install github.com/ramonvermeulen/whosthere@latest
+go install github.com/sachaos/tcpterm@latest
+go install github.com/surge-downloader/surge@latest
+go install github.com/szktkfm/mdtt/cmd/mdtt@latest
+go install github.com/tannerr-dev/bt-cli@latest
+go install github.com/tantalor93/dnspyre/v3@latest
+go install github.com/TheZoraiz/ascii-image-converter@latest
 go install github.com/ycd/dstp/cmd/dstp@latest
-go install github.com/codeshaine/curlify@latest
+go install github.com/Zebbeni/ansizalizer@latest
+go install github.com/Zxilly/go-size-analyzer/cmd/gsa@latest
+go install go.dalton.dog/prism@latest
 
 # rm -rf /usr/local/go && tar -C /usr/local -xzf go1.21.4.linux-amd64.tar.gz
 
--- Install/Python.sh
+++ Install/Python.sh
@@ -3,34 +3,4 @@ RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m' 	# No Color
 
-pipx install ansible
-pipx install ansible-core
-pipx install ansible-dev-tools
-pipx install ansible-lint
-pipx install ansible-navigator
-pipx install ansible-playbook-grapher
-pipx install borgbackup
-pipx install bpytop
-pipx install frogmouth
-pipx install marker-pdf
-pipx install mkdocs-ansible
-pipx install molecule
-pipx install recoverpy
-pipx install sshclick
-pipx install terminaltexteffects
-pipx install textual-paint
-pipx install tftui
-pipx install tiptop
-pipx install uv
-pipx install visidata
-pipx install pywal
-pipx install shodan
-pipx install bdfr
-pipx install waybackpack
-pipx install pmbootstrap
-pipx install paramiko
-pipx install argcomplete
-pipx install tk
-pipx install bbot
-pipx install jrnl
-pipx install maigret
+pipx install 0-0-1, ansible, ansible-core, ansible-dev-tools, ansible-lint, ansible-navigator, ansible-playbook-grapher, anthropic, argcomplete, aria2tui, art, ascii-guard, bbot, bdfr, blender-mcp, borgbackup, bpytop, charset-normalizer, cinecli, dooit, frogmouth, glances, hoardy-adb, howmanypeoplearearound, hwinfo-tui, jmespath, jrnl, listpick, maigret, marker-pdf, mcp-server-git, mkdocs-ansible, mmrl-util, molecule, n2g, netshow, nvitop, oslo-core, paramiko, peewee, pmbootstrap, protontricks, pytest-sugar, pywal, recoverpy, shodan, sibylline-scurl, solaar, sot, spotdl, sqlit-tui, sshclick, streamlink, tailscale-network-topology-mapper, terminaltexteffects, textual-paint, tftui, tiptop, tk, tofuref, uv, visidata, waybackpack, wtfis, xart
