287c9e7a18ddf5a8d00d4ad8127c760b9d9525e8
matrix8967 <2366621-matrix8967@users.noreply.gitlab.com>
2026-08-26 08:31:24 -0500

    * Updates dot files for Zed, Zsh, and Kitty. * Adds new modified script for MXErgo S.

---
+++ Misc/xinput/MXErgo_S.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+DeviceID="$(xinput |grep -m 1 "Logitech USB Receiver Mouse "| cut -d"=" -f 2| cut -d "[" -f 1)"
+
+xinput --set-button-map $DeviceID 1 2 3 4 5 6 7 8 2 10 11 12
+xinput --set-prop $DeviceID "libinput Button Scrolling Button" 9
+xinput --set-prop $DeviceID "libinput Scroll Method Enabled" 0 0 1
+xinput --set-prop $DeviceID 'libinput Accel Speed' 1
